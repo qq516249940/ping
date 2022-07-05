e7fada51f267a44365a0b87139326e65d32bda01
yancong <516249940@qq.com>
2022-07-05 15:58:54 +0800

    ping成功 bot企业微信通知

---
+++ go.mod
@@ -0,0 +1,11 @@
+module ping
+
+go 1.18
+
+require github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e
+
+require (
+	github.com/vimsucks/wxwork-bot-go v0.0.0-20210525031914-b584a24ed8c5 // indirect
+	golang.org/x/net v0.0.0-20220630215102-69896b714898 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)
---
+++ ping.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
+	wxworkbot "github.com/vimsucks/wxwork-bot-go"
+)
+
+func ping(x string) {
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", x)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+		bot()
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func bot() {
+	bot := wxworkbot.New(os.Args[2])
+	// or Markdown, Image, News
+
+	// 文本消息
+	// text := wxworkbot.Text{
+	// 	Content:             "Hello World",
+	// 	MentionedList:       []string{"foo;", "bar"},
+	// 	MentionedMobileList: []string{"@all"},
+	// }
+	// err := bot.Send(text)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Markdown 消息
+
+	markdown := wxworkbot.Markdown{
+		Content: "# 可以ping通192.168.201.73地址了",
+	}
+	err := bot.Send(markdown)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func main() {
+	ping(os.Args[1])
+
+}
